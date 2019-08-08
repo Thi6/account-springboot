06429259b3d7b14701ae598b88938f15c44efdeb
Thi6 <thi.dnguyen96@gmail.com>
2019-08-08 09:48:30 +0100

    added react frontend

---
+++ account-react/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+import './App.css';
+import AccountForm from './components/AccountForm';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <AccountForm />
+      </div>
+    );
+  }
+
+}
+
+export default App;
